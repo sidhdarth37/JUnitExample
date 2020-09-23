f29a426aa961a5bfe4da9570d551e860ee17d425
sidhdarth37 <52353093+sidhdarth37@users.noreply.github.com>
2020-09-23 11:32:45 +0530

    Add files via upload

--- Calc.java
+++ Calc.java
@@ -1,4 +1,4 @@
-package emp;
+
 
 public class Calc {
 	public int calcSum(int a , int b)
@@ -8,5 +8,16 @@ public class Calc {
 	public int calcDiff(int a,int b)
 	{
 		return a-b;
+	}
+	public int calcDiv(int a,int b)
+	{
+		return a/b;
+	}
+	public int calcMult(int a,int b)
+	{
+		long l=((long)( a))*b;
+		if(l>Integer.MAX_VALUE)
+			throw new ArithmeticException();
+		return a*b;
 	}
 }
--- TestCalc.java
+++ TestCalc.java
@@ -1,4 +1,4 @@
-package emp;
+
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -28,6 +28,52 @@ public class TestCalc {
 		int exp=3;
 		int act=c.calcDiff(10, 7);
 		Assert.assertEquals(exp, act);
+	}
+	
+	@Test
+	public void testCalcDivValid()
+	{
+		System.out.println("Testing Div");
+		Calc c=new Calc();
+		int exp=12;
+		int act=c.calcDiv(120, 10);
+		Assert.assertEquals(exp, act);
+	}
+	
+	@Test
+	public void testCalcMult()
+	{
+		System.out.println("Testing Mult");
+		Calc c=new Calc();
+		int exp=120;
+		int act=c.calcMult(12, 10);
+		Assert.assertEquals(exp, act);
+	}
+	
+	@Test
+	public void testCalcMultByZero()
+	{
+		System.out.println("Testing Mult by 0");
+		Calc c=new Calc();
+		int exp=0;
+		int act=c.calcMult(12, 0);
+		Assert.assertEquals(exp, act);
+	}
+	
+	@Test(expected= ArithmeticException.class)
+	public void testCalcMultLarge()
+	{
+		System.out.println("Testing Mult for large values");
+		Calc c=new Calc();
+		int act=c.calcMult(1000000007, 100000009);
+	}
+	
+	@Test(expected= ArithmeticException.class)
+	public void testCalcDivException()
+	{
+		System.out.println("Expecting Exception");
+		Calc c=new Calc();
+		c.calcDiv(12, 0);
 	}
 	
 	@Before
